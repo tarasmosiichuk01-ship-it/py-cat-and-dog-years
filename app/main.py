84fa131e7e7e5fd7e1a1e7998a9c69e3190ff8eb
Taras Mosiichuk <taras.mosiichuk.01@gmail.com>
2026-01-01 10:10:01 +0200

    Solution3

--- app/main.py
+++ app/main.py
@@ -18,7 +18,6 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
         get_human_age(15, 15) == [1, 1]
         get_human_age(24, 24) == [2, 2]
     """
-    # TODO: Implement this function
 
     if not isinstance(cat_age, int) or not isinstance(dog_age, int):
         raise TypeError
